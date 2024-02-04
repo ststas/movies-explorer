53b3515ae445faec6a14cfad4ee6fe28db648069
Stas Zaitsev <ststas@yandex.ru>
2024-02-04 20:00:00 +0200

    update constants

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,23 +1,40 @@
-export const BEATFILM_URL = "https://api.nomoreparties.co/beatfilm-movies"
-export const BASE_BEATFILM_URL = "https://api.nomoreparties.co"
-// export const BASE_USER_URL = "https://api.movie.nomoredomainsicu.ru"
-export const BASE_USER_URL = "http://localhost:4000"
+export const BEATFILM_URL = "https://api.nomoreparties.co/beatfilm-movies";
+export const BASE_BEATFILM_URL = "https://api.nomoreparties.co";
+export const BASE_USER_URL = "https://api.movie.nomoredomainsicu.ru";
 
-export const ICONS = ["HTML", "CSS", "JS", "React", "Git", "Express.js", "mongoDB"];
+export const ICONS = [
+  "HTML",
+  "CSS",
+  "JS",
+  "React",
+  "Git",
+  "Express.js",
+  "mongoDB",
+];
 
 export const PORTFOLIO = [
-  {name: "Desktop Website", link: "https://ststas.github.io/projects/how-to-learn"},
-  {name: "Responsive Website", link: "https://ststas.github.io/projects/russian-travel"},
-  {name: "Single Page Application", link: "https://ststas.github.io/projects/mestoreact"},
-]
+  {
+    name: "Desktop Website",
+    link: "https://ststas.github.io/projects/how-to-learn",
+  },
+  {
+    name: "Responsive Website",
+    link: "https://ststas.github.io/projects/russian-travel",
+  },
+  {
+    name: "Single Page Application",
+    link: "https://ststas.github.io/projects/mestoreact",
+  },
+];
 
-export const LARGE_SCREEN = 1280
-export const MID_SCREEN = 768
-export const LARGE_SCREEN_MOVIES = 12
-export const MID_SCREEN_MOVIES = 8
-export const SMALL_SCREEN_MOVIES = 5
-export const LARGE_SCREEN_ADD_MOVIES = 3
-export const MID_SCREEN_ADD_MOVIES = 2
+export const LARGE_SCREEN = 1280;
+export const MID_SCREEN = 768;
+export const LARGE_SCREEN_MOVIES = 12;
+export const MID_SCREEN_MOVIES = 8;
+export const SMALL_SCREEN_MOVIES = 5;
+export const LARGE_SCREEN_ADD_MOVIES = 3;
+export const MID_SCREEN_ADD_MOVIES = 2;
 
-export const RegExPassword = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"
-export const PasswordError = "Password must be at least 8 characters including a lowercase letter, an uppercase letter, and a number."
+export const RegExPassword = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$";
+export const PasswordError =
+  "Password must be at least 8 characters including a lowercase letter, an uppercase letter, and a number.";
